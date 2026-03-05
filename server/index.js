1ae2dc49adb2d0178480702484d69f02eb79a8d2
janaf7620 <janaf7620@gmail.com>
2026-03-05 10:30:00 +0100

    Dodata autentifikacija i registracija korisnika

--- server/index.js
+++ server/index.js
@@ -8,13 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require('./routes/authRoutes');
-const propertyRoutes = require('./routes/propertyRoutes');
-const clientRoutes = require('./routes/clientRoutes');
 
 // Rute
 app.use('/api/auth', authRoutes);
-app.use('/api/properties', propertyRoutes);
-app.use('/api/clients', clientRoutes);
 
 // Health Check rutua
 app.get('/api/health', (req, res) => {
